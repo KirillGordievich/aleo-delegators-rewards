0af8494920c59b5ef803ce403d2c7ffc5981a73e
Kirill <kirill.gordievich777@gmail.com>
2023-11-30 15:13:05 +0300

    WIP

---
+++ sql/migrations/1.0.0-initial/rollback/0003_init.aleo_rewards.entities.rollback.sql
@@ -0,0 +1,10 @@
+BEGIN;
+
+--
+--  Drop tables
+--
+DROP TABLE IF EXISTS aleo_rewards.transaction_info;
+DROP TABLE IF EXISTS aleo_rewards.block_info;
+DROP TABLE IF EXISTS aleo_rewards.delegator_info;
+DROP TABLE IF EXISTS aleo_rewards.validator_info;
+
