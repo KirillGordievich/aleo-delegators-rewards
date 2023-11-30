f121a9982c0f94a71068a5cf92932067fb9a454f
Kirill <kirill.gordievich777@gmail.com>
2023-11-30 15:28:10 +0300

    WIP (return original error msg)

--- src/aleo_api_client.py
+++ src/aleo_api_client.py
@@ -13,42 +13,42 @@ class AleoNetworkApiClient:
             response.raise_for_status()
             return response.json()
         except Exception as error:
-            raise Exception("Error getting data")
+            raise Exception(f"Error getting data: {str(error)}")
 
     def getBlock(self, height: int) -> Union[Block, Exception]:
         try:
             block = self.makeRequest("/block/" + str(height))
             return block
         except Exception as error:
-            raise Exception("Error getting block")
+            raise Exception(f"Error getting block: {str(error)}")
 
     def getBlockRange(self, start: int, end: int) -> Union[List[Block], Exception]:
         try:
             return self.makeRequest("/blocks?start=" + str(start) + "&end=" + str(end))
         except Exception as error:
-            error_message = f"Error getting blocks between {start} and {end}"
+            error_message = f"Error getting blocks between {start} and {end}: {str(error)}"
             raise Exception(error_message)
 
     def getLatestBlock(self) -> Union[Block, Exception]:
         try:
             return self.makeRequest("/latest/block")
         except Exception as error:
-            raise Exception("Error getting latest block")
+            raise Exception(f"Error getting latest block: {str(error)}")
 
     def getLatestHeight(self) -> Union[int, Exception]:
         try:
             return self.makeRequest("/latest/height")
         except Exception as error:
-            raise Exception("Error getting latest height")
+            raise Exception(f"Error getting latest height: {str(error)}")
 
     def getProgramMappingValue(self, programId: str, mappingName: str, key: str) -> Union[str, Exception]:
         try:
             return self.makeRequest("/program/" + programId + "/mapping/" + mappingName + "/" + key)
         except Exception as error:
-            raise Exception("Error getting mapping value - ensure the mapping exists and the key is correct")
+            raise Exception(f"Error getting mapping value - ensure the mapping exists and the key is correct: {str(error)}")
 
     def getTransactions(self, height: int) -> Union[List[TransactionModel], Exception]:
         try:
             return self.makeRequest("/block/" + str(height) + "/transactions")
         except Exception as error:
-            raise Exception("Error getting transactions")
+            raise Exception(f"Error getting transactions: {str(error)}")
