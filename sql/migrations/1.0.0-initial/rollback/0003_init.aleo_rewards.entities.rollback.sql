738e6a8aa82acfb862573a23c4233f44a5a3f626
Kirill <kirill.gordievich777@gmail.com>
2023-12-04 18:36:15 +0300

    WIP (fix naming and new constraints)

--- sql/migrations/1.0.0-initial/rollback/0003_init.aleo_rewards.entities.rollback.sql
+++ sql/migrations/1.0.0-initial/rollback/0003_init.aleo_rewards.entities.rollback.sql
@@ -3,8 +3,10 @@ BEGIN;
 --
 --  Drop tables
 --
-DROP TABLE IF EXISTS aleo_rewards.transaction_info;
-DROP TABLE IF EXISTS aleo_rewards.block_info;
-DROP TABLE IF EXISTS aleo_rewards.delegator_info;
-DROP TABLE IF EXISTS aleo_rewards.validator_info;
+DROP TABLE IF EXISTS aleo_rewards.stake_transaction;
+DROP TABLE IF EXISTS aleo_rewards.block_height;
+DROP TABLE IF EXISTS aleo_rewards.address;
+
+DROP TYPE IF EXISTS aleo_rewards.stake_function;
+DROP TYPE IF EXISTS aleo_rewards.address_type;
 
